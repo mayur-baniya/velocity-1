4993c9b4a7fe84b65f1e985161812f0a95c6ca3b
ItzMeAbhi <ak203332@gmail.com>
2024-01-27 23:39:32 +0530

    day1

---
+++ lib/autentications/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Home",
+        ),
+      ),
+    );
+  }
+}
